eb2bc6848bbed1401b1249c76c3346006e78878f
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2024-07-04 08:08:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.3 MB (100.00%)

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int prod = 1;
+        int sum = 0;
+        while(n!=0){
+            int digit = n%10 ;
+            sum=sum+digit;
+            prod  = prod*digit;
+            n=n/10;
+        }
+        int result=prod-sum;
+        return result;
+        
+    }
+};
