8f1e544c2a1d27e4e48a1bc5d3b6649fb1a885cf
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2024-07-05 05:41:51 +0000

    Sync LeetCode submission - Reverse Integer - Runtime - 0 ms (100.00%), Memory - 7.4 MB (100.00%)

--- 0007-reverse-integer/solution.cpp
+++ 0007-reverse-integer/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int reverse(int x) {
-        int rev = 0;
+        int ans=0;
         while(x!=0){
-            int temp = x%10;
-             // Check for overflow/underflow before updating ans
-            if ((rev > INT_MAX / 10) || (rev < INT_MIN / 10)) {
-                return 0; // Return 0 if reversing x would cause overflow/underflow
-            }
-
-            rev = rev*10 + temp;
-            x = x/10;
+            int digit= x%10;
+            if(ans>(INT_MAX/10) || ans<(INT_MIN/10)){
+                return 0;
+            } 
+            
+            ans=(ans*10)+digit;
+            x=x/10;
         }
-        return rev;
+         // check range if r is outside the range then return 0  
+        
+        return ans;
+
         
     }
 };
