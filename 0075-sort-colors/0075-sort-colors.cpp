ad6b6b70e70372c16a0e585c860c513ac12b69b9
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2024-10-01 15:51:12 +0530

    Time: 0 ms (100%), Space: 10.3 MB (68.5%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int min = i;
+            for(int j = i+1;j<n;j++)
+            {
+                if(nums[j]<nums[min]){
+                    swap(nums[j],nums[min]);
+                }
+            }
+        }
+        
+    }
+};
