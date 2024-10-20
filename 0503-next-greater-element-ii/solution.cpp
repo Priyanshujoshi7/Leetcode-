d522fc710a8f2d07823c871c5994b1c18bda0cf8
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2024-10-20 14:42:38 +0000

    Sync LeetCode submission - Next Greater Element II - Runtime - 8 ms (25.28%), Memory - 26.7 MB (100.00%)

--- 0503-next-greater-element-ii/solution.cpp
+++ 0503-next-greater-element-ii/solution.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, -1); 
-        stack<int> st;  
-        for (int i = 0; i < 2 * n; i++) {
-            int currentIndex = i % n;  
-            while (!st.empty() && nums[currentIndex] > nums[st.top()]) {
-                ans[st.top()] = nums[currentIndex];  
-                st.pop();  
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=0; i<2*n; i++){
+            int idx = i%n;
+            while(!st.empty() && nums[st.top()]<nums[idx]){
+                ans[st.top()] = nums[idx];
+                st.pop();
             }
-            if (i < n) {
+            if(i<n){
                 st.push(i);
             }
         }
         return ans;
+
     }
 };
