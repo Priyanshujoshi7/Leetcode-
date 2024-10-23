e4a77e44b83ac570d40b133e2ffec6535f4177c6
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2024-10-23 08:38:33 +0000

    Sync LeetCode submission - Sum of Two Integers - Runtime - 0 ms (100.00%), Memory - 7.3 MB (100.00%)

--- 0371-sum-of-two-integers/solution.cpp
+++ 0371-sum-of-two-integers/solution.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
-    
     int getSum(int a, int b) {
-        int carry;
-        while(b != 0){
-            carry = a & b;
-            a = a ^ b;
-            b = carry<<1;
-        }
-        return a;   
+
+        return a+b;
+        
     }
 };
