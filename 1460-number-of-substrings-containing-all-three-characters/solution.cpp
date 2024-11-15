7a49bbd894f4d59c7664d8936f2e36278684167f
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2024-11-15 07:20:27 +0000

    Sync LeetCode submission Runtime - 58 ms (5.44%), Memory - 11.2 MB (28.48%)

---
+++ 1460-number-of-substrings-containing-all-three-characters/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n =s.size();
+
+        int l=0, r =0;
+        int count = 0;
+        unordered_map<char,int> mpp;
+        while(r<n){
+            mpp[s[r]]++;
+            while(mpp['a']>=1 && mpp['b']>=1 && mpp['c']>=1){
+                count+= n-r;
+                mpp[s[l]]--;
+                l++;
+            }
+            r++;
+        }
+        return count;
+
+
+        
+    }
+};
