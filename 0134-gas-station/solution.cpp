38a28bdd3bb0f7c3a7752e88f58836ef34182c98
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2024-11-23 16:49:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 111.1 MB (99.98%)

---
+++ 0134-gas-station/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int totalGas =0;
+        int totalCost = 0;
+        for(int i=0;i<n;i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if(totalCost>totalGas){
+            return -1;
+        }
+        int currentGas= 0;
+        int start=0;
+        for(int i=0;i<n;i++){
+            currentGas += gas[i]-cost[i];
+            if(currentGas<0){
+                currentGas =0;
+                start = i+1;
+            }
+        }
+        return start;
+        
+    }
+};
