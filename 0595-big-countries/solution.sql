5c112af5762ac44596ed5c432cfa48143e33060b
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2025-12-12 19:42:18 +0000

    Sync LeetCode submission - Big Countries - Runtime - 309 ms (59.78%), Memory - 0.0B (100.00%)

--- 0595-big-countries/solution.sql
+++ 0595-big-countries/solution.sql
@@ -1,2 +1,17 @@
 # Write your MySQL query statement below
-select name ,population,area from world where area>=3000000 or population>=25000000;
+WITH Answer_table AS(
+    SELECT
+        name,
+        population,
+        area
+    FROM World
+    WHERE area >= 3000000
+    OR population >= 25000000
+)
+
+SELECT
+    name,
+    population,
+    area
+FROM Answer_table;
+
