9577ae06ef30b3b5a339196bdd9ac00e12d53a9d
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-04-09 09:36:48 +0000

    Sync LeetCode submission Runtime - 318 ms (98.28%), Memory - 0.0B (100.00%)

---
+++ 0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    name
+FROM Employee
+WHERE id IN(
+    SELECT
+        managerID
+    FROM Employee
+    GROUP BY managerID
+    HAVING COUNT(managerID)>=5
+)
