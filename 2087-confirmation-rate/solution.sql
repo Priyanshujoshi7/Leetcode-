482423d749bfad2dc4b876af940cef34d9a9910a
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-04-10 06:12:14 +0000

    Sync LeetCode submission Runtime - 684 ms (61.51%), Memory - 0.0B (100.00%)

---
+++ 2087-confirmation-rate/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    s.user_id,
+    round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+FROM Signups as s
+LEFT JOIN Confirmations as c
+ON s.user_id = c.user_id
+Group by s.user_id;
