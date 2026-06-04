6b2ad65e9602830af63cd3b0ef6a63edb4df9328
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-06-04 04:39:31 +0000

    Sync LeetCode submission Runtime - 454 ms (88.97%), Memory - 0.0B (100.00%)

---
+++ 1278-product-price-at-a-given-date/solution.sql
@@ -0,0 +1,28 @@
+WITH cte AS (
+    SELECT
+        product_id,
+        new_price,
+        RANK() OVER (
+            PARTITION BY product_id
+            ORDER BY change_date DESC
+        ) AS r
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
+
+SELECT
+    product_id,
+    new_price AS price
+FROM cte
+WHERE r = 1
+
+UNION
+
+SELECT DISTINCT
+    product_id,
+    10 AS price
+FROM Products
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM cte
+);
