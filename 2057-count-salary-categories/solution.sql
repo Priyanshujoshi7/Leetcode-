d6e66ababd75bc90f724a415f49f9105bbf839fa
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-06-09 04:49:14 +0000

    Sync LeetCode submission Runtime - 1600 ms (45.68%), Memory - 0.0B (100.00%)

---
+++ 2057-count-salary-categories/solution.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    "Low Salary" AS category,
+    COUNT(if(income<20000,1,null)) AS accounts_count
+FROM Accounts
+UNION ALL
+SELECT
+    "Average Salary",
+    COUNT(if(income<=50000 AND income>=20000,1,null)) AS accounts_count
+FROM Accounts
+UNION ALL
+SELECT
+    "High Salary",
+    COUNT(if(income>50000,1,null)) AS accounts_count
+FROM Accounts;
