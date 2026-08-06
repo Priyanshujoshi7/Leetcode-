86e88a8530da990a0f2f266a58dd971f443b9c88
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-08-06 05:49:07 +0000

    Sync LeetCode submission Runtime - 466 ms (31.59%), Memory - 0.0B (100.00%)

---
+++ 0175-combine-two-tables/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    p.firstName AS firstName,
+    p.lastName AS lastName,
+    a.city AS city,
+    a.state AS state
+FROM Person as p
+LEFT JOIN Address as a
+ON p.personId = a.personID;
