acbb0d51640a2703c26838e9a7ba0895661fe335
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-08-11 07:35:58 +0000

    Sync LeetCode submission - Find Customer Referee - Runtime - 460 ms (91.71%), Memory - 0.0B (100.00%)

--- 0584-find-customer-referee/solution.sql
+++ 0584-find-customer-referee/solution.sql
@@ -1,12 +1,4 @@
-# Write your MySQL query statement below
-WITH answer_table AS(
-    SELECT
-        name 
-    FROM Customer
-    WHERE referee_id <> 2
-    OR referee_id IS NULL
-)
-
-SELECT 
-    name
-FROM answer_table;
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL
+   OR referee_id != 2;
