9028fb1e3f9c2ce15e8e74563f13c4fb727aa8c6
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-08-20 06:05:11 +0000

    Sync LeetCode submission - Customers Who Bought All Products - Runtime - 655 ms (44.26%), Memory - 0.0B (100.00%)

--- 1135-customers-who-bought-all-products/solution.sql
+++ 1135-customers-who-bought-all-products/solution.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
 SELECT
     customer_id
-FROM customer
+FROM customer 
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) = (
-    SELECT COUNT(*)
-    FROM Product   
-);
+HAVING COUNT(DISTINCT product_key) = (SELECT count(*) FROM Product);
 
