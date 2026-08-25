ad30abe9baa20914f10d4b1fd88248deb6074038
Priyanshu <166523704+Priyanshujoshi7@users.noreply.github.com>
2026-08-25 05:29:42 +0000

    Sync LeetCode submission - Trips and Users - Runtime - 597 ms (67.83%), Memory - 0.0B (100.00%)

--- 0262-trips-and-users/solution.sql
+++ 0262-trips-and-users/solution.sql
@@ -1,30 +1,22 @@
-WITH valid_trips AS (
-    SELECT
-        t.request_at AS Day,
-        t.status
-    FROM Trips t
-    JOIN Users c
-        ON t.client_id = c.users_id
-        AND c.banned = 'No'
-    JOIN Users d
-        ON t.driver_id = d.users_id
-        AND d.banned = 'No'
-    WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-),
-daily AS (
-    SELECT
-        Day,
-        COUNT(*) OVER (PARTITION BY Day) AS total_trips,
+# Write your MySQL query statement below
+SELECT
+    t.request_at AS Day,
+    ROUND(
         SUM(
             CASE
-                WHEN status IN ('cancelled_by_client', 'cancelled_by_driver')
+                WHEN t.status IN ('cancelled_by_client', 'cancelled_by_driver')
                 THEN 1
                 ELSE 0
             END
-        ) OVER (PARTITION BY Day) AS cancelled_trips
-    FROM valid_trips
-)
-SELECT DISTINCT
-    Day,
-    ROUND(cancelled_trips / total_trips, 2) AS `Cancellation Rate`
-FROM daily;
+        ) / COUNT(*),
+        2
+    ) AS `Cancellation Rate`
+FROM Trips t
+JOIN Users c
+    ON t.client_id = c.users_id
+    AND c.banned = 'No'
+JOIN Users d
+    ON t.driver_id = d.users_id
+    AND d.banned = 'No'
+WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at;
